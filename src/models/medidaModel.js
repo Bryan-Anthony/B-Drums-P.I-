ee11a81595a0c724d02be498646e4840adde83c5
Bryan-Anthony <bryan.amoraes@sptech.school>
2025-06-03 14:16:53 -0300

    atualização botão quiz.

--- src/models/medidaModel.js
+++ src/models/medidaModel.js
@@ -3,7 +3,7 @@ var database = require("../database/config");
 function buscarUltimasMedidas() {
     // SELECT KPIS
     // OPERAÇÕES MATEMÁTICAS
-    
+
     var instrucaoSql = `SELECT 
     ROUND((AVG(CASE WHEN pergunta = 'sim' THEN 1 ELSE 0 END) * 100), 2) AS media_sim,
     ROUND((AVG(CASE WHEN pergunta = 'não' THEN 1 ELSE 0 END) * 100), 2) AS media_nao,
